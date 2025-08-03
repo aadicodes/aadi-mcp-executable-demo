833471a6195757fcdb4859ef214c6b50b93e199c
aadicodes <rajeshaadi@gmail.com>
2025-08-03 08:19:57 -0700

    checking working code

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # aadi-mcp-executable-demo
-This project has code and instructions to deploy a github mcp server using python sdk
+This project has code and instructions to deploy a github mcp server using python sdk. 
+This uses FastMCP framework. This is a simple demo and all code is present in server.py file.
+
+## Steps before executing the code
+To ensure you have the right libraries and tools follow below steps.
+1. Install Python v12 or above
+2. pip install FastMCP
+3. Install Claude Desktop, follow this link for downloading appropriate version https://claude.ai/download
+4. Install uv, the pyhton package manager, instructions are available at https://github.com/astral-sh/uv
+
+Now take a deep breath, you installed lot of new stuff, they all need to work together. 
+
+# Steps to install MCP server and test with Claude Desktop
+1. Run below command to install MCP server to Claude Desktop
+```uv run mcp install server.py```
+
+
---
+++ mcp-server-demo/server.py
@@ -0,0 +1,50 @@
+"""
+FastMCP quickstart example.
+This example demonstrates how to create a simple MCP server using FastMCP.
+It includes a tool for addition, static and dynamic resources, and a prompt.
+Run this script to start the server and access the tools and resources.
+Make sure to have FastMCP installed in your environment. Checkout Readme.md for more details.
+
+"""
+
+from mcp.server.fastmcp import FastMCP
+
+
+# Create an MCP server
+mcp = FastMCP("local_mcp", "1.0.0")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+@mcp.tool()
+def happy_greeting(name: str) -> str:
+    return f"Good Day, {name} , from Local MCP server!"
+
+# Add a static greeting resource
+@mcp.resource("daymessage://greet")
+def get_greeting() -> str:
+    """Get a personalized greeting"""
+    return f"Good Day, my FRIEND , from Local MCP server static Resource !"
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Good Day, {name} , from Local MCP server Resource Template!"
+
+
+# Add a prompt
+@mcp.prompt()
+def greet_user(name: str, style: str = "friendly") -> str:
+    """Generate a greeting prompt"""
+    styles = {
+        "friendly": "Please write a warm, friendly greeting",
+        "formal": "Please write a formal, professional greeting",
+        "casual": "Please write a casual, relaxed greeting",
+    }
+
+    return f"{styles.get(style, styles['friendly'])} for someone named {name}."
